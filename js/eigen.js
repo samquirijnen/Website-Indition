249d6f5b03cbda0c6da00b4788073b402f171d23
Sam Quirijnen <squirijnen@gmail.com>
2020-10-17 08:51:05 +0200

    update

--- js/eigen.js
+++ js/eigen.js
@@ -9,6 +9,11 @@
    /* ColorAndStylepagina*/
 
    /* Gallerijpagina*/
+   $(document).ready(function () {
+      $("#nanoGallery3").nanoGallery({
+          itemsBaseURL:'https://brisbois.fr/nanogallery/demonstration/'
+      });
+  });
 
    /* Prijslijstpagina*/
 
